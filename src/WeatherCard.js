cc378b02bb9ac74c961da9f75d52a8cf692aff15
Cin1983! <cinnymaria@gmail.com>
2021-01-02 22:40:37 +0100

    update 2 januari 2021

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -1,43 +1,43 @@
 import React from "react";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
-
-
-const WeatherCard = ({ city, country, weatherIcon, main,celcius, temp_min, temp_max,description }) => {
-    return (
-        <div className="container">
-            <div className="div cards">
-                <h1>{city}, {country}</h1>
-                <h5 className="py-4">
-                    <i className={` fas fa-bolt ${weatherIcon} `} />
-                </h5>
-                <h1 className="py-2">{celcius}&deg;</h1>
-                {/**add minMaxTemp */}
-                {minMaxTemp(temp_min, temp_max)}
-                <h4 className="py-3">{description}</h4>
-            </div>
-
-
-        </div>
-
-        
-    )
-}
+const WeatherCard = ({
+  city,
+  country,
+  weatherIcon,
+  celcius,
+  temp_min,
+  temp_max,
+  description,
+}) => {
+  return (
+    <div className="container">
+      <div className="cards pt-4">
+        <h1>
+          {city}, {country}
+        </h1>
+        <h5 className="py-4">
+          <i className={`wi ${weatherIcon} display-1`} />
+        </h5>
+        <h1 className="py-2">{celcius}&deg;</h1>
+        {/**add minMaxTemp */}
+        {minMaxTemp(temp_min, temp_max)}
+        <h4 className="py-3">{description}</h4>
+      </div>
+    </div>
+  );
+};
 
 function minMaxTemp(min, max) {
-    return (
-        <h3>
-            <span className="px-4">{min}&deg;</span>
-            <span className="px-4">{max}&deg;</span>
-        </h3>
-    )
-    
+  return (
+    <h3>
+      <span className="px-4">{min}&deg;</span>
+      <span className="px-4">{max}&deg;</span>
+    </h3>
+  );
 }
-
 
 
 
 export default WeatherCard;
-
-
--- src/index.js
+++ src/index.js
@@ -1,105 +1,114 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "weather-icons/css/weather-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WeatherCard from "./WeatherCard";
+import Form from "./Form";
 
-const API_key ='ffc50fde0210deb85d418dc51cb7e5e6'
-;
+const API_key = "ffc50fde0210deb85d418dc51cb7e5e6";
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            city: undefined,
-            country: undefined,
-            icon: undefined,
-            main: undefined,
-            celcius: undefined,
-            temp_max: undefined,
-            tem_min: undefined,
-            description: "",
-            error: false
-            
-        };
-        this.getWeather();
+  constructor() {
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celcius: undefined,
+      temp_max: undefined,
+      tem_min: undefined,
+      description: "",
+      error: false,
+    };
+    this.getWeather();
 
-        this.weatherIcon = {
-            bolt: '',
-            wind: '',
-            cloud: '',
-            smog: '',
-            sun: '',
-            thunderstorm: '',
-            snowflakes: '',
-            raindrups:''
-      }
-    
+    this.weatherIcon = {
+      Thunderstorm: " wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: " wi-storm-showers",
+      Snow: "wi-snow",
+      Atmospere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog",
+    };
+  }
+
+  calCelcius(temp) {
+    let cell = Math.floor(temp - 273.15);
+    return cell;
+  }
+
+  get_weatherIcon(icons, rangeId) {
+    switch (true) {
+      case rangeId >= 200 && rangeId <= 232:
+        this.setState({ icon: this.weatherIcon.Thunderstorm });
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: this.weatherIcon.Drizzle });
+        break;
+      case rangeId >= 500 && rangeId <= 531:
+        this.setState({ icon: this.weatherIcon.Rain });
+        break;
+
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: this.weatherIcon.Snow });
+        break;
+
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: this.weatherIcon.Atmospere });
+        break;
+
+      case rangeId === 800:
+        this.setState({ icon: this.weatherIcon.Clear });
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: this.weatherIcon.Clouds });
+        break;
+      default:
+        this.setState({ icon: this.weatherIcon.Clouds });
     }
+  }
 
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=Amsterdam,nl &appid=${API_key}`
+    );
 
-
-
-    calCelcius(temp) {
-        let cell = Math.floor(temp - 273.15);
-        return cell;
-}
-
-
-
-
-
-
-    getWeather = async () => {
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Amsterdam,nl &appid=${API_key}`
-           
-        );
-    
     const response = await api_call.json();
-        console.log(response)
-        this.setState({
-            city: response.name,
-            country: response.sys.country,
-            celcius: this.calCelcius(response.main.temp),
-            temp_min: this.calCelcius(response.main.temp_min),
-            temp_max: this.calCelcius(response.main.temp_max),
-            description: response.weather[0].description,
-            weatherIcon: this.weatherIcon.bolt
-        })
-    
-}
 
-    
-    
-    render() {
-        return <WeatherCard
+    console.log(response);
+
+    this.setState({
+      city: response.name,
+      country: response.sys.country,
+      celcius: this.calCelcius(response.main.temp),
+      temp_min: this.calCelcius(response.main.temp_min),
+      temp_max: this.calCelcius(response.main.temp_max),
+      description: response.weather[0].description,
+    });
+
+    this.get_weatherIcon(this.weatherIcon, response.weather[0].id);
+  };
+
+  render() {
+      return (
+          <div className="App">
+        <Form />
+          
+      <WeatherCard
             city={this.state.city}
-            country={this.state.country} 
+            country={this.state.country}
             celcius={this.state.celcius}
             temp_min={this.state.temp_min}
             temp_max={this.state.temp_max}
             description={this.state.description}
-            weatherIcon={this.state.weatherIcon}
-            />;
+            weatherIcon={this.state.icon}
+        />
+       </div> 
+    );
   }
 }
-
-// class App extends React.Component {
-//     state = { temp: [] };
-
-//     onClickSubmit = async term => {
-//         const response_weather = await postMessage.get("api.openweathermap.org/data/2.5/weather?id=2172797&appid={fbc42b71a1f25bdcec4ba6705cfcb970}
-//             params: { id: term }
-
-//         });
-
-//     render() {
-//         return (
-//             <div>
-
-//             </div>
-//         )
-//     }
-// }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 
